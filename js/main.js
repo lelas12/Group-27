f57c1b227177f20c03959c11fec99f862bc78e02
lisaajansson <jali23sf@student.ju.se>
2024-01-08 18:43:09 +0100

    new js

--- js/main.js
+++ js/main.js
@@ -3,3 +3,15 @@ function changeImage(newSrc) {
   }
 
   let getUp = () => window.scrollTo({top:0, behavior:'smooth'});
+
+
+  
+  let popup = document.getElementById("popup-box");
+ 
+  function openPopup(){
+    popup.classList.add("open-popup");
+  }
+  function closePopup(){
+    popup.classList.remove("open-popup");
+  }
+
